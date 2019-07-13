b457ab036f8164a03f85585ec6974e0a363df3f0
Ryusei Takano <s1250034@gmail.com>
2019-07-14 01:28:24 +0900

    first commit

---
+++ OCRSwift/View/ViewController.swift
@@ -0,0 +1,10 @@
+import UIKit
+import SwiftyJSON
+import AVFoundation
+import Alamofire
+
+class ViewController: UIViewController, AVCapturePhotoCaptureDelegate{
+    override func viewDidLoad() {
+        super.viewDidLoad()
+    }
+}
